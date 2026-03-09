65430079b3aff60ce09903099af3f598ab342477
Benita <theexceptionalpm@gmail.com>
2026-03-09 22:12:35 +0100

    fix: resolve alembic multiple head revisions - unique revision ID and correct down_revision

--- backend/alembic/versions/add_scheduling_fields_to_meetings.py
+++ backend/alembic/versions/add_scheduling_fields_to_meetings.py
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'd4e5f6a7b8c9'
-down_revision: Union[str, Sequence[str], None] = 'c3d4e5f6a7b8'
+revision: str = 'g7h8i9j0k1l2'
+down_revision: Union[str, Sequence[str], None] = 'b1c2d3e4f5g6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
